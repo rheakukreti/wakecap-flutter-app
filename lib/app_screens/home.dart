82f7fc2076c013e1fc2e30974c28bf0373c84ecc
rheakukreti <rheakukreti221@gmail.com>
2019-07-30 01:00:56 +0530

    Fixed image URL dimensions, elongated appBar

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -4,6 +4,13 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:gradient_app_bar/gradient_app_bar.dart';
 
+
+var apiUrl = "http://api.emiratesauction.com/v2/carsonline";
+List cars;
+Map jsonData;
+List<String> dim = ["[w]", "[h]"], val = ["0", "0"];
+Map<String, String> map = new Map.fromIterables(dim, val);
+
 class Home extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -13,9 +20,6 @@ class Home extends StatefulWidget {
 
 class MainUI extends State<Home> {
 
-  var apiUrl = "http://api.emiratesauction.com/v2/carsonline";
-  List cars;
-  Map jsonData;
 
   Future getCar() async {
     http.Response data = await http.get(apiUrl);
@@ -46,139 +50,189 @@ class MainUI extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    List <String> dim = ["[w]", "[h]"];
-    List <double> val = [300.0, 300.0];
-    Map dimensions = new Map.fromIterables(dim, val);
-
-    return Scaffold(
-        appBar: GradientAppBar(
-          title: Text("Cars and Machinery"),
-          backgroundColorStart: Colors.pink,
-          backgroundColorEnd: Colors.deepOrange,
-          leading: IconButton(
-              icon: Icon(
-                Icons.arrow_back,
-                color: Colors.white,
-              ),
-              onPressed: null),
-          actions: <Widget>[Icon(Icons.language)],
-        ),
-        body: RefreshIndicator(
-            key: refreshKey,
-            onRefresh: refresh,
-            child: Column(children: <Widget>[
-
-             Card( elevation: 5.0,
-               child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                FlatButton.icon(
-                  padding: EdgeInsets.only(top: 25.0, bottom: 25.0, left: 10.0, right: 10.0),
-                  icon: Icon(Icons.filter_list),
-                  label: Text("Filter"),
-                ),
-
-                FlatButton.icon(
-                  padding: EdgeInsets.only(top: 25.0, bottom: 25.0, left: 10.0, right: 10.0),
-                  icon: Icon(Icons.sort),
-                  label: Text("Sort"),
-                ),
-
-                FlatButton.icon(
-                  padding: EdgeInsets.only(top: 25.0, bottom: 25.0, left: 10.0, right: 10.0),
-                  icon: Icon(Icons.view_module),
-                  label: Text("Grid View"),
-                ),
-
-              ],),),
-
-              Expanded(child: ListView.builder(
-              itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  height: 180.0,
-                  margin: EdgeInsets.only(
-                      left: 10.0, top: 10.0, right: 10.0, bottom: 10.0),
-                  child: Card(
-                    elevation: 5.0,
-                    child: Row(
-                      children: <Widget>[
-                        Column(
-                          children: <Widget>[
-
-                            Stack(children: <Widget>[
-                              Image.network(
-                                cars[index]["image"],
-                                height: 170.0,
-                                width: 150.0,
-                              ),
-
-//                              IconButton(
-//
-//                                icon: Icon(Icons.add_circle_outline),
-//                                color: likeColor,
-//                                onPressed: () {
-//                                  setState(() {
-//                                    likeColor = Colors.red;
-//                                  });
-//                                },
-//
-//                              )
-
-                            ],)
-                          ],
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            new Text(cars[index]["makeEn"] +
-                                " " +
-                                cars[index]["modelEn"] +
-                                " " +
-                                cars[index]["year"].toString(), style: TextStyle(
-                              fontSize: 20.0,
-                            ),),
-                            new Text(cars[index]["AuctionInfo"]
-                            ["currentPrice"]
-                                .toString() +
-                                " " +
-                                cars[index]["AuctionInfo"]["currencyEn"], style: TextStyle(
-                              fontSize: 30.0,
-                            ),),
-                            Row(
-                              mainAxisAlignment:
-                              MainAxisAlignment.spaceEvenly,
-                              children: <Widget>[
-                                Column(
-                                  children: <Widget>[
-                                    Text("Lot #"),
-                                    Text(cars[index]["AuctionInfo"]["lot"]
-                                        .toString())
-                                  ],
-                                ),
-                                Column(
-                                  children: <Widget>[
-                                    Text("Bids"),
-                                    Text(cars[index]["AuctionInfo"]["bids"]
-                                        .toString())
-                                  ],
-                                ),
-                                Column(
-                                  children: <Widget>[
-                                    Text("Time left"),
-                                    Text(cars[index]["AuctionInfo"]
-                                    ["endDateEn"])
-                                  ],
-                                )
-                              ],
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),)],)
-            ));
+    return MaterialApp(
+      title: 'Cars and Machinery',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: appBarDesign(),
+          body: RefreshIndicator(
+              key: refreshKey,
+              onRefresh: refresh,
+              child: Column(
+                children: <Widget>[
+                  topBar(),
+                  scrollText(),
+           carsList(),
+                ],
+              ))),
+    );
   }
+}
+
+Widget appBarDesign() {
+  return PreferredSize(
+    preferredSize: Size.fromHeight(100.0),
+    child: GradientAppBar(
+      title: Text("Cars and Machinery"),
+      backgroundColorStart: Colors.pink,
+      backgroundColorEnd: Colors.deepOrange,
+      centerTitle: true,
+      leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: null),
+    ),
+  );
+}
+
+Widget topBar() {
+  return Card(
+    elevation: 5.0,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        FlatButton.icon(
+          padding:
+              EdgeInsets.only(top: 25.0, bottom: 25.0, left: 10.0, right: 10.0),
+          icon: Icon(Icons.filter_list),
+          label: Text("Filter", style: TextStyle(color: Colors.black)),
+        ),
+        Container(
+          height: 55.0,
+          width: 1.0,
+          color: Colors.grey,
+        ),
+        FlatButton.icon(
+          padding:
+              EdgeInsets.only(top: 25.0, bottom: 25.0, left: 10.0, right: 10.0),
+          icon: Icon(Icons.sort),
+          label: Text(
+            "Sort",
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        Container(
+          height: 55.0,
+          width: 1.0,
+          color: Colors.grey,
+        ),
+        FlatButton.icon(
+          padding:
+              EdgeInsets.only(top: 25.0, bottom: 25.0, left: 10.0, right: 10.0),
+          icon: Icon(Icons.view_module),
+          label: Text("Grid View", style: TextStyle(color: Colors.black)),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget scrollText() {
+  return Container(
+    margin: EdgeInsets.only(top: 10.0, bottom: 5.0),
+    child: Text(
+      "Scroll down to search",
+      style: TextStyle(color: Colors.grey),
+    ),
+  );
+}
+
+String fixDimensions(String x) {
+  return map.entries.fold(x, (prev, e) => prev.replaceAll(e.key, e.value));
+}
+
+Widget carsList(){
+
+  return Expanded(child: ListView.builder(
+    itemCount: cars == null ? 0 : cars.length,
+    itemBuilder: (BuildContext context, int index) {
+      return Container(
+        height: 150.0,
+        margin: EdgeInsets.only(
+            left: 10.0,top: 10.0,bottom: 10.0, right: 10.0),
+        child: Card(
+          elevation: 5.0,
+          child: Row(
+            children: <Widget>[
+              Expanded(flex: 5,
+                child: Container(foregroundDecoration: BoxDecoration(image: DecorationImage(image: NetworkImage(fixDimensions(cars[index]["image"])),
+                    fit: BoxFit.fill
+                ),) ),
+
+
+              ),
+              Expanded(
+                flex: 6,
+                child: Container(margin: EdgeInsets.only(left: 10.0, right: 10.0),
+                  child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment
+                      .spaceEvenly,
+                  children: <Widget>[
+                     Text(cars[index]["makeEn"] +
+                        " " +
+                        cars[index]["modelEn"] +
+                        " " +
+                        cars[index]["year"].toString(),
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),),
+                     Text(cars[index]["AuctionInfo"]
+                    ["currentPrice"]
+                        .toString() +
+                        " " +
+                        cars[index]["AuctionInfo"]["currencyEn"],
+                      style: TextStyle(
+                        fontSize: 30.0,
+                      ),),
+                    Row(crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment:
+                      MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Column(crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text("Lot #"),
+                             Text(cars[index]["AuctionInfo"]["lot"]
+                                .toString())
+                          ],
+                        ),
+
+                        Container(
+                          height: 25.0,
+                          width: 1.0,
+                          color: Colors.grey,
+                        ),
+                        Column(crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text("Bids"),
+                            Text(
+                                cars[index]["AuctionInfo"]["bids"]
+                                    .toString())
+                          ],
+                        ),
+
+                        Container(
+                          height: 25.0,
+                          width: 1.0,
+                          color: Colors.grey,
+                        ),
+
+                        Column(crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text("Time left"),
+
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),) ),
+            ],
+          ),
+        ),
+      );
+    },
+  ),);
+
 }
