74314610e1b56a1254e23af05eaab88e5d722fb7
rheakukreti <rheakukreti221@gmail.com>
2019-07-30 17:53:24 +0530

    Optimized text fields and added external fonts

--- README.md
+++ README.md
@@ -1,16 +1,3 @@
-# wakecap_flutter_app
+Wake Cap Flutter App
 
-A new Flutter application.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+This is a single page app to view aution information of cars obtained through an API as a scrollable list. The exercise explores multiple concepts like json parsing, asynchronous programming, dealing with long lists, widget nesting etc. 
--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -4,14 +4,13 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:gradient_app_bar/gradient_app_bar.dart';
 
-
 var apiUrl = "http://api.emiratesauction.com/v2/carsonline";
 List cars;
 Map jsonData;
 List<String> dim = ["[w]", "[h]"], val = ["0", "0"];
 Map<String, String> map = new Map.fromIterables(dim, val);
 String flag = "En";
-var textDir=TextDirection.ltr;
+var textDir = TextDirection.ltr;
 
 class Home extends StatefulWidget {
   @override
@@ -21,10 +20,10 @@ class Home extends StatefulWidget {
 }
 
 class MainUI extends State<Home> {
-
-  var lang=["English", "عربى"];
+  var lang = ["English", "عربى"];
   var currentItem = "English";
 
+  //Get future data from API
   Future getCar() async {
     http.Response data = await http.get(apiUrl);
 
@@ -33,10 +32,9 @@ class MainUI extends State<Home> {
     setState(() {
       cars = jsonData["Cars"];
     });
-
   }
 
-
+  //Define initial state of app
   @override
   void initState() {
     super.initState();
@@ -46,6 +44,7 @@ class MainUI extends State<Home> {
   final GlobalKey<RefreshIndicatorState> refreshKey =
       new GlobalKey<RefreshIndicatorState>();
 
+  //Future state after refresh
   Future<Null> refresh() {
     return getCar().then((_cars) {
       setState(() {
@@ -53,21 +52,30 @@ class MainUI extends State<Home> {
       });
     });
   }
+
   @override
   Widget build(BuildContext context) {
+    //Main layout of the app
 
     return MaterialApp(
       title: 'Cars and Machinery',
       debugShowCheckedModeBanner: false,
-
       home: Scaffold(
+
+          //Appbar layout
           appBar: PreferredSize(
             preferredSize: Size.fromHeight(100.0),
             child: GradientAppBar(
-              title: Text("Cars and Machinery"),
+              title: Text(
+                "Cars and Machinery",
+                style: TextStyle(
+                    fontFamily: 'RobotoCondensed', fontWeight: FontWeight.w700),
+              ),
               backgroundColorStart: Colors.pink,
               backgroundColorEnd: Colors.deepOrange,
               centerTitle: true,
+
+              //Language button on appbar
               leading: IconButton(
                   icon: Icon(
                     Icons.arrow_back,
@@ -75,81 +83,56 @@ class MainUI extends State<Home> {
                   ),
                   onPressed: null),
               actions: <Widget>[
-                DropdownButton<String>(icon: Icon(Icons.language, color: Colors.white, size: 30.0,),
-
-                  underline: SizedBox(width: 0.0,),
-
-                  items: lang.map((String menuItem){
+                DropdownButton<String>(
+                  icon: Icon(
+                    Icons.language,
+                    color: Colors.white,
+                    size: 30.0,
+                  ),
+                  underline: SizedBox(
+                    width: 0.0,
+                  ),
+                  items: lang.map((String menuItem) {
                     return DropdownMenuItem<String>(
                       value: menuItem,
                       child: Text(menuItem),
                     );
                   }).toList(),
 
-                  onChanged: (String selectedValue){
-
-                  setState(() {
-                    this.currentItem=selectedValue;
-                    currentItem=="English"?flag="En":flag="Ar";
-                  });
-
-                  debugPrint(flag);
-
+                  //Changing language values through dropdown menu
+                  onChanged: (String selectedValue) {
+                    //
+                    setState(() {
+                      this.currentItem = selectedValue;
+                      currentItem == "English" ? flag = "En" : flag = "Ar";
+                    });
                   },
-
                 )
               ],
             ),
           ),
+
+          //App body beyond appbar
           body: RefreshIndicator(
               key: refreshKey,
               onRefresh: refresh,
               child: Column(
                 children: <Widget>[
+                  //Top bar widget
                   topBar(),
+
+                  //Scroll to refresh test widget
                   scrollText(),
-           carsList(),
+
+                  //Car information long list widget
+                  carsList(),
                 ],
               ))),
     );
   }
 }
 
-//Widget appBarDesign() {
-//  return PreferredSize(
-//    preferredSize: Size.fromHeight(100.0),
-//    child: GradientAppBar(
-//      title: Text("Cars and Machinery"),
-//      backgroundColorStart: Colors.pink,
-//      backgroundColorEnd: Colors.deepOrange,
-//      centerTitle: true,
-//      leading: IconButton(
-//          icon: Icon(
-//            Icons.arrow_back,
-//            color: Colors.white,
-//          ),
-//          onPressed: null),
-//      actions: <Widget>[
-//        DropdownButton<String>(icon: Icon(Icons.language),
-//
-//          items: lang.map((String menuItem){
-//            return DropdownMenuItem<String>(
-//              value: menuItem,
-//              child: Text(menuItem),
-//            );
-//          }).toList(),
-//
-//          onChanged: (String selectedValue){
-//
-//
-//          },
-//
-//        )
-//      ],
-//    ),
-//  );
-//}
-
+//Top bar widget
 Widget topBar() {
   return Card(
     elevation: 5.0,
@@ -160,7 +143,11 @@ Widget topBar() {
           padding:
               EdgeInsets.only(top: 25.0, bottom: 25.0, left: 10.0, right: 10.0),
           icon: Icon(Icons.filter_list),
-          label: Text("Filter", style: TextStyle(color: Colors.black)),
+          label: Text("Filter",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontFamily: 'RobotoCondensed',
+                  fontWeight: FontWeight.w700)),
         ),
         Container(
           height: 55.0,
@@ -173,7 +160,10 @@ Widget topBar() {
           icon: Icon(Icons.sort),
           label: Text(
             "Sort",
-            style: TextStyle(color: Colors.black),
+            style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.w700),
           ),
         ),
         Container(
@@ -185,13 +175,18 @@ Widget topBar() {
           padding:
               EdgeInsets.only(top: 25.0, bottom: 25.0, left: 10.0, right: 10.0),
           icon: Icon(Icons.view_module),
-          label: Text("Grid View", style: TextStyle(color: Colors.black)),
+          label: Text("Grid View",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontFamily: 'RobotoCondensed',
+                  fontWeight: FontWeight.w700)),
         ),
       ],
     ),
   );
 }
 
+//Scroll to refresh test widget
 Widget scrollText() {
   return Container(
     margin: EdgeInsets.only(top: 10.0, bottom: 5.0),
@@ -206,96 +201,150 @@ String fixDimensions(String x) {
   return map.entries.fold(x, (prev, e) => prev.replaceAll(e.key, e.value));
 }
 
-Widget carsList(){
-
-  return Expanded(child: ListView.builder(
-    itemCount: cars == null ? 0 : cars.length,
-    itemBuilder: (BuildContext context, int index) {
-      return Container(
-        height: 150.0,
-        margin: EdgeInsets.only(
-            left: 10.0,top: 10.0,bottom: 10.0, right: 10.0),
-        child: Card(
-          elevation: 5.0,
-          child: Row(
-            children: <Widget>[
-              Expanded(flex: 5,
-                child: Container(foregroundDecoration: BoxDecoration(image: DecorationImage(image: NetworkImage(fixDimensions(cars[index]["image"])),
-                    fit: BoxFit.fill
-                ),) ),
-
-
-              ),
-              Expanded(
-                flex: 6,
-                child: Container(margin: EdgeInsets.only(left: 10.0, right: 10.0),
-                  child: Column(crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment
-                      .spaceEvenly,
-                  children: <Widget>[
-                     Text(cars[index]["make$flag"] +
-                        " " +
-                        cars[index]["model$flag"] +
-                        " " +
-                        cars[index]["year"].toString(),
-                      style: TextStyle(
-                        fontSize: 20.0,
-                      ),),
-                     Text(cars[index]["AuctionInfo"]
-                    ["currentPrice"]
-                        .toString() +
-                        " " +
-                        cars[index]["AuctionInfo"]["currency$flag"],
-                      style: TextStyle(
-                        fontSize: 30.0,
-                      ),),
-                    Row(crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment:
-                      MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Column(crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text("Lot #",),
-                             Text(cars[index]["AuctionInfo"]["lot"]
-                                .toString())
-                          ],
-                        ),
-
-                        Container(
-                          height: 25.0,
-                          width: 1.0,
-                          color: Colors.grey,
-                        ),
-                        Column(crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text("Bids"),
-                            Text(
-                                cars[index]["AuctionInfo"]["bids"]
-                                    .toString())
-                          ],
-                        ),
-
-                        Container(
-                          height: 25.0,
-                          width: 1.0,
-                          color: Colors.grey,
-                        ),
-
-                        Column(crossAxisAlignment: CrossAxisAlignment.start,
-                           children: <Widget>[
-                            Text("Time left"),
-
-                          ],
-                        )
-                      ],
-                    )
-                  ],
-                ),) ),
-            ],
+//Car information long list widget
+Widget carsList() {
+  return Expanded(
+    child: ListView.builder(
+      itemCount: cars == null ? 0 : cars.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          height: 150.0,
+          margin:
+              EdgeInsets.only(left: 10.0, top: 10.0, bottom: 10.0, right: 10.0),
+          child: Card(
+            elevation: 5.0,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 5,
+                  child: Container(
+                      foregroundDecoration: BoxDecoration(
+                    image: DecorationImage(
+                        image:
+                            NetworkImage(fixDimensions(cars[index]["image"])),
+                        fit: BoxFit.fill),
+                  )),
+                ),
+                Expanded(
+                    flex: 6,
+                    child: Container(
+                      margin: EdgeInsets.only(left: 10.0, right: 10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Text(
+                            cars[index]["make$flag"] +
+                                " " +
+                                cars[index]["model$flag"] +
+                                " " +
+                                cars[index]["year"].toString(),
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                fontFamily: 'RobotoCondensed',
+                                fontWeight: FontWeight.w700),
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Text(
+                                cars[index]["AuctionInfo"]["currentPrice"]
+                                    .toString(),
+                                style: TextStyle(
+                                    color: Colors.green,
+                                    fontSize: 28.0,
+                                    fontFamily: 'RobotoCondensed',
+                                    fontWeight: FontWeight.w400),
+                              ),
+                              Text(
+                                cars[index]["AuctionInfo"]["currency$flag"],
+                                textAlign: TextAlign.start,
+                                style: TextStyle(
+                                  fontSize: 12.0,
+                                  color: Colors.green,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text(
+                                    "Lot #",
+                                    style: TextStyle(
+                                        fontFamily: 'RobotoCondensed',
+                                        fontWeight: FontWeight.w300,
+                                        color: Colors.grey),
+                                  ),
+                                  Text(
+                                    cars[index]["AuctionInfo"]["lot"]
+                                        .toString(),
+                                    style: TextStyle(
+                                        fontFamily: 'RobotoCondensed',
+                                        fontWeight: FontWeight.w700),
+                                  )
+                                ],
+                              ),
+                              Container(
+                                height: 25.0,
+                                width: 1.0,
+                                color: Colors.grey,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text(
+                                    "Bids",
+                                    style: TextStyle(
+                                        fontFamily: 'RobotoCondensed',
+                                        fontWeight: FontWeight.w300,
+                                        color: Colors.grey),
+                                  ),
+                                  Text(
+                                    cars[index]["AuctionInfo"]["bids"]
+                                        .toString(),
+                                    style: TextStyle(
+                                        fontFamily: 'RobotoCondensed',
+                                        fontWeight: FontWeight.w700),
+                                  )
+                                ],
+                              ),
+                              Container(
+                                height: 25.0,
+                                width: 1.0,
+                                color: Colors.grey,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text(
+                                    "Time left",
+                                    style: TextStyle(
+                                        fontFamily: 'RobotoCondensed',
+                                        fontWeight: FontWeight.w300,
+                                        color: Colors.grey),
+                                  ),
+                                  Text(
+                                    "00:00:00",
+                                    style: TextStyle(
+                                        fontFamily: 'RobotoCondensed',
+                                        fontWeight: FontWeight.w700),
+                                  )
+                                ],
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    )),
+              ],
+            ),
           ),
-        ),
-      );
-    },
-  ),);
-
+        );
+      },
+    ),
+  );
 }
